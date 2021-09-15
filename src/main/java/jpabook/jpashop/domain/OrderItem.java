668017e0251ae555c0eabbc248fe92837f354576
hjoon <guswns3371@gmail.com>
2021-09-15 17:14:27 +0900

    5. 주문 도메인 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,11 +1,22 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
+/**
+ * protected OrderItem() {
+ * }
+ * -> new OrderItem()으로 생성을 막고, createOrderItem()으로 생성하도록 유도할 수 있다
+ *
+ * @NoArgsConstructor(access = AccessLevel.PROTECTED)
+ * 어노테이션을 통해 protected 생성자를 생략할 수 있다.
+ */
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Setter
 @Getter
 @Entity
@@ -27,4 +38,36 @@ public class OrderItem {
     private int orderPrice; // 주문 당시 가격
     private int count; // 주문 당시 수량
 
+    /**
+     * 생성 메소드
+     */
+
+
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        // OrderItem 을 생성하면서 재고를 까준다
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    /**
+     * 비지니스 로직
+     * 주문 취소
+     */
+    public void cancel() {
+        // 주문 당시 수량 만큼 다시 재고를 올려준다
+        getItem().addStock(count);
+    }
+
+    /**
+     * 조회 로직
+     * 주문 상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,76 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    /**
+     * 주문
+     */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+        // 엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        // 배송 정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        // 주문 상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // 주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 주문 저장 -> OrderItem, Delivery 가 자동으로 persist 됨 (CASCADE.ALL 덕분)
+        // OrderItem, Delivery 는 Order만 참조하기 때문에 CASCADE를 사용할 수 있다.
+        // 만약 다른 곳에서도 OrderItem, Delivery를 참조한다면 CASCADE 옵션을 사용하지 말 것.
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        // 주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+
+        // 주문 취소
+        // order.cancel()
+        // -> 배송중인 경우 예외 발생, orderItem.cancel();
+        // -> getItem().addStock(count); 재고 수량 만큼 add
+        order.cancel();
+    }
+
+    /**
+     * 검색
+     */
+    public List<Order> findOrders(OrderSearch searchOrder) {
+        return orderRepository.findAll(searchOrder);
+    }
+}
